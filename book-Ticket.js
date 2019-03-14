2d8502a6d3681e1bad5308f96d006cd5e59cdc62
Sowndharya13 <sowndharyakanakaraj@gmail.com>
2019-03-14 16:11:35 +0530

    Created ticket assest and network card successfully

--- book-Ticket.js
+++ book-Ticket.js
@@ -10,13 +10,14 @@
  * Create a new ticket resource instance and add it to the ticket Registry
  */
  // Parameter is a AssetRegistry
+ var tNumber = 100;
 
  const bnUtil = require('../bn-connection-util');
  const ticketNamespace = 'org.acme.airline.ticket';
  const transactionType = 'BookTicket';
    
     // Change this for populating other versions
-bnUtil.cardName='admin@airlinev10';
+bnUtil.cardName='admin@airlinev12';
 if(process.argv.length < 3){
     console.log("Usage: node populate-acme-airline   <card-name> ")
     console.log("Populating Network using a card: ",bnUtil.cardName);
@@ -27,37 +28,34 @@ if(process.argv.length < 3){
 bnUtil.connect(main);
     // Check ticket availability 
     function checkAvailability(){
-    
-
     // Write a query to check seat availablity in the given flight
-  
     }
     function main(error){
         if(error){
             console.log(error)
             process.exit(1)
         }
-        bookTicket('AE101-12-09-2019',4,'FIRSTCLASS','EWR','ATL', new Date() ,'SRM-Agencies');
+        bookTicket('AE101-12-09-2019',4,'FIRSTCLASS','EWR','ATL','SRM-Agencies');
     }
-    
-    function bookTicket(flightId, numberOfSeatsToReserve, seatClass, origin, destination, date, key){
+    function bookTicket(flightId, numberOfSeatsToReserve, seatClass, origin, destination, partnerAgencyName){
      //if available..generate ticketId and mark the tickets
-              
-    
-
      let tickets = [];
      const  bnDef = bnUtil.connection.getBusinessNetwork();
      const  factory = bnDef.getFactory();
+     var bookingTime = new Date().getTime();
 
+     ticket.ticketNumber = date + tNumber++  ;
+
+     var ticketId = ticket.ticketNumber +"-" +bookingTime;
      let transaction = factory.newTransaction(ticketNamespace,transactionType); 
-    // transaction.setPropertyValue('ticketNumber',number);
+     transaction.setPropertyValue('ticketNumber',ticketNumber);
      transaction.setPropertyValue('flightId',flightId);
      transaction.setPropertyValue('numberOfSeatsToReserve',numberOfSeatsToReserve);
      transaction.setPropertyValue('seatClass',seatClass);
      transaction.setPropertyValue('orgin', origin);
      transaction.setPropertyValue('destination' , destination );
-     transaction.setPropertyValue('schedule' , date);
-     transaction.setPropertyValue('participantKey', key);
+     transaction.setPropertyValue('timeOfBooking' , bookingTime);
+     transaction.setPropertyValue('partnerAgencyName', partnerAgencyName);
      
      bnUtil.connection.submitTransaction(transaction).then(()=>{
         console.log('Successfully booked a new ticket  : ',number);
@@ -67,5 +65,6 @@ bnUtil.connect(main);
     }).catch((error)=>{
         console.log(error);
         bnUtil.connection.disconnect();
-    })
-}  
+    })         
+}             
+                
